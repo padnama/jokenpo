c78a79576e7f4cdc4b01c60915b95c15652c23c4
padnama <amandacepereira@gmail.com>
2022-05-30 13:06:39 -0300

    Add player selection, comparison and result

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
+/*
+1. playerSelection
+  a. prompt player selection
 
-// call function computerPlay
-// make variable randomNumber that selects a random number
-// if the random number is 0, return rock
-// if it's 1, return paper
-// else, return scissors
+2. computerSelection
+  a. call function computerPlay
+  b. make variable randomNumber that selects a random number
+  c. if the random number is 0, return rock
+  d. if it's 1, return paper
+  e. else, return scissors 
+  
+3. compare selections
 
-function computerPlay() {
+4. output result
+
+*/
+
+let playerSelection = prompt("Rock, paper ou scissors?").toLowerCase();
+
+let computerPlay = function() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
-    return 'Rock';
+    return "rock";
   }
   else if (randomNumber === 1) {
-    return 'Paper';
+    return "paper";
   }
   else {
-    return 'Scissors';
+    return "scissors";
   }
 }
 
-console.log(computerPlay())
+let compare = function(choice1, choice2) {
+  if(choice1 === choice2) {
+    console.log("It's a tie!");
+  }
+  else if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+      console.log("Rock beats scissors!");
+    } else {
+      console.log("Paper beats rock!");
+    }
+  }
+  else if(choice1 === "paper") {
+    if(choice2 === "rock") {
+      console.log("Paper beats rock!");
+    } else {
+      console.log("Scissors beat paper");
+    }
+  }
+  else if(choice1 === "scissors") {
+    if(choice2 === "rock") {
+      console.log("Rock beats scissors!");
+    } else {
+      console.log("Scissors beat paper");
+    }
+  }
+}
+
+console.log(compare(playerSelection, computerPlay));
