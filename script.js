f0a6b57380e73f1c34ab6107e92c63b4f69dda4b
padnama <amandacepereira@gmail.com>
2022-05-30 16:25:23 -0300

    Add loop for five rounds

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-/*
-1. playerSelection
-  a. prompt player selection
-
-2. computerSelection
-  a. call function computerPlay
-  b. make variable randomNumber that selects a random number
-  c. if the random number is 0, return rock
-  d. if it's 1, return paper
-  e. else, return scissors 
-  
-3. compare selections
-
-4. output result
-
-*/
-
-let playerSelection = prompt("Rock, paper ou scissors?").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
 let computerPlay = function() {
   let randomNumber = Math.floor(Math.random() * 3);
@@ -31,30 +15,52 @@ let computerPlay = function() {
 }
 
 let compare = function(choice1, choice2) {
-  if(choice1 === choice2) {
-    console.log("It's a tie!");
+  if(choice1 === choice2) { // if choice one equals choice two,
+    console.log("It's a tie!"); // let user know it's a tie
   }
-  else if(choice1 === "rock") {
-    if(choice2 === "scissors") {
-      console.log("Rock beats scissors!");
-    } else {
-      console.log("Paper beats rock!");
+  else if(choice1 === "rock") { // however, if choice one is rock,
+    if(choice2 === "scissors") { // evaluate whether choice two is scissors. in this case,
+      console.log("You win!!"); // let user know they win
+    } else { // otherwise,
+      console.log("You lose!"); // let user know they lose
     }
   }
   else if(choice1 === "paper") {
     if(choice2 === "rock") {
-      console.log("Paper beats rock!");
+      console.log("You win!");
     } else {
-      console.log("Scissors beat paper");
+      console.log("You lose!");
     }
   }
   else if(choice1 === "scissors") {
     if(choice2 === "rock") {
-      console.log("Rock beats scissors!");
+      console.log("You lose!");
     } else {
-      console.log("Scissors beat paper");
+      console.log("You win!");
     }
   }
 }
 
-console.log(compare(playerSelection, computerPlay));
+
+
+let game = function() {
+  for (let i = 0; i < 5; i++) {
+    
+    let playerSelection = prompt("Rock, paper ou scissors?").toLowerCase();
+
+    compare(playerSelection, computerPlay);
+      if (compare === "You win!") {
+       playerScore++;
+      }
+      else if (compare === "You lose!") {
+        computerScore++;
+      }
+      else if (compare === "It's a tie!") {
+  
+      }
+
+ }
+}
+
+game();
+console.log("Your score is: " + playerScore);
